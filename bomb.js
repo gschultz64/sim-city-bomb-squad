6fa9e16f68716ce71cba914d6d76b2c118d49c19
Genevieve Schultz <genschultz@gmail.com>
2018-05-25 14:51:55 -0700

    partially complete game

--- bomb.js
+++ bomb.js
@@ -1,6 +1,84 @@
 console.log("javascript running");
 
+var startingTime = 30;
+var remainingTime = 0;
+var gameOver = false;
+var wiresToCut = [];
+
+var delay = null;
+
+
+var wiresCut = {
+  blue: false,
+  green: false,
+  red: false,
+  white: false,
+  yellow: false
+}
+
+var cutWire = function() {
+  if (!wiresCut[this.id] && !gameOver) {
+    // Do the wire cutting and game checking here
+    this.src = "img/cut-" + this.id + "-wire.png";
+    wiresCut[this.id] = true;
+    // Check for correct wire
+    var wireIndex = wiresToCut.indexOf(this.id);
+    if (wireIndex > -1) {
+      // This is where the good cut logic goes
+      console.log(this.id + " was correct!");
+      wiresToCut.splice(wireIndex, 1);
+      if (checkForWin()) {
+        endGame();
+      }
+    } else {
+      // this is where the bad cut logic goes
+      console.log(this.id + " was incorrect!");
+      
+    }
+  } 
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM loaded");
+  for (let wire in wiresCut) {
+    document.getElementById(wire).addEventListener("click", cutWire);
+  }
+  
 });
 
+
+
+//On load -
+//  Start timer
+//  Class unexploded
+//  Display uncut wire images
+//  Color of timer "red"
+//  Deactivate reset button
+//  Play siren sound
+//  Randomly assign correct wires
+
+//On Win -
+//  Timer stops
+//  Color of timer "green"
+//  Play "yay" sound
+//  Play victory song
+//  Enable reset button
+
+//On bad wire cut -
+//  Start 750ms timeout
+//  Change class to exploded
+//  Enable reset button
+//  Play "boom" sound
+//  Timer stops
+//  On timeout, do previous 4 steps
+
+//On any wire cut -
+//  Swap image
+//  Disable click
+//  Is this correct or wrong wire?
+//    Correct: final wire? if so, win.
+//    Wrong: start timeout
+
+//Keep track of wires that have been cut
+//Need to know which wires need to be cut
+//Wires in array, remove from array when cut("clicked")
